2519a1ff90e1011c3636604a057437088054577c
LuisDaniel <luisdaniellaserna@gmail.com>
2026-09-09 12:51:38 +0800

    FEAT: use poster trivia rotating every 8s on loading screen

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -6,6 +6,6 @@ import TriviaCard from './TriviaCard.vue'
   <div class="flex flex-col items-center justify-center gap-4 mt-20 px-4">
     <span class="loading loading-spinner loading-lg text-primary"></span>
     <p class="text-lg opacity-80">Loading your quiz...</p>
-    <TriviaCard :interval-ms="0" :refresh-on-mount="false" />
+    <TriviaCard :interval-ms="8000" :refresh-on-mount="false" variant="poster" />
   </div>
 </template>
