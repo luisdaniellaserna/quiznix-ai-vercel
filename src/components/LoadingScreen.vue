5c919d7496f05a7d6c6a06064bd0b66d4dc7b60f
LuisDaniel <luisdaniellaserna@gmail.com>
2026-09-09 11:42:50 +0800

    FEAT: show stable trivia card on loading screen

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -1,6 +1,11 @@
+<script lang="ts" setup>
+import TriviaCard from './TriviaCard.vue'
+</script>
+
 <template>
-  <div class="flex flex-col items-center justify-center gap-4 mt-20">
+  <div class="flex flex-col items-center justify-center gap-4 mt-20 px-4">
     <span class="loading loading-spinner loading-lg text-primary"></span>
     <p class="text-lg opacity-80">Loading your quiz...</p>
+    <TriviaCard :interval-ms="0" :refresh-on-mount="false" />
   </div>
 </template>
